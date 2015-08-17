c22e797a686920191d529da6c3a3663541036537
mdunbavan <mdunbavan@gmail.com>
2015-08-17 15:12:11 +0100

    debug mode

--- single-case-studies.php
+++ single-case-studies.php
@@ -13,12 +13,11 @@ $p = get_the_content();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">	
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
+<?php echo $p; ?>
 		<div class="entry-content up">
 			<header class="entry-header">
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 			</header><!-- .entry-header -->
-			<?php echo $p; ?>
 			<?php
 				wp_link_pages( array(
 					'before' => '<div class="page-links">' . __( 'Pages:', '_mbbasetheme' ),
