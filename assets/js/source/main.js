1f8a7435c0217c5e8626850974aaf64bdd7ff74a
mdunbavan <mdunbavan@gmail.com>
2015-08-18 17:25:34 +0100

    responsive images on related work not giving way to text fixed

--- assets/js/source/main.js
+++ assets/js/source/main.js
@@ -657,6 +657,7 @@
 			infinite: true,
 			slidesToShow: 4,
 			slidesToScroll: 2,
+			dots: false,
 			responsive: [
 			    {
 			      breakpoint: 1024,
@@ -664,21 +665,23 @@
 			        slidesToShow: 3,
 			        slidesToScroll: 3,
 			        infinite: true,
-			        dots: true
+			        dots: false
 			      }
 			    },
 			    {
 			      breakpoint: 600,
 			      settings: {
 			        slidesToShow: 2,
-			        slidesToScroll: 2
+			        slidesToScroll: 2,
+			        dots: false
 			      }
 			    },
 			    {
 			      breakpoint: 480,
 			      settings: {
 			        slidesToShow: 1,
-			        slidesToScroll: 1
+			        slidesToScroll: 1,
+			        dots: false
 			      }
 			    }
 			    // You can unslick at a given breakpoint now by adding:
