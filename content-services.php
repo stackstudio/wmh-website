decc8ad766f444baab5faba2b8fe228e31ee619d
mdunbavan <mdunbavan@gmail.com>
2015-09-03 20:59:58 +0100

    changed up to down

--- content-services.php
+++ content-services.php
@@ -8,7 +8,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('work-block'); ?>>
 
-	<div class="entry-content up">
+	<div class="entry-content down">
 		<?php the_content(); ?>
 	</div><!-- .entry-content -->
 	<article id="who-we-are" class="generic-bg work-block" style="background-image: url(<?php _basetheme_post_thumbnail_helper(); ?>);">
